a09da9b142a3d8279ec216dc753e0622f31176ac
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-05-29 14:38:34 +0200

    Add previous issues/PRs to the PR/issue description.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # update-tester
+
+Make sure that Actions can create and approve pull requests:
+
+Go to Settings -> Actions -> General and select "Allow GitHub Actions to create and approve pull requests".
+
---
+++ do-update/scripts/listPreviousPRs.mjs
@@ -0,0 +1,33 @@
+import core from "@actions/core";
+import { execSync } from "child_process";
+
+/**
+ * Turn an array of issue/PR numbers into a bulleted list. For example:
+ * 	[1, 37, 1234]
+ * becomes:
+ * 	"""* 1
+ * 	* 37
+ * 	* 1234"""
+ *
+ * The resulting string has no leading or trailing whitespace (such as linebreaks).
+ */
+function formatIssueList(issueNumbers) {
+	return issueNumbers.map(number => `* ${number}`).join("\n");
+}
+
+const openIssues = JSON.parse(execSync('gh issue list --label "auto-update-lean" --state open --json number'))
+	.map(issue => issue.number);
+const openPRs = JSON.parse(execSync('gh pr list --label "auto-update-lean" --state open --json number'))
+	.map(issue => issue.number);
+
+core.setOutput("previous-issues-exist", openIssues.length > 0);
+core.setOutput("previous-prs-exist", openPRs.length > 0);
+
+var summaryText = '';
+if (openPRs.length > 0) {
+	summaryText += `\n\nPrevious unmerged auto-update PRs:\n${formatIssueList(openPRs)}`
+}
+if (openIssues.length > 0) {
+	summaryText += `\n\nPrevious open auto-update issues:\n${formatIssueList(openPRs)}`
+}
+core.setOutput("summary-text", summaryText);
